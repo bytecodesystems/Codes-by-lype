93dcc2a2b9349f551e75b01342384d8396a1a65b
lypegabryel7 <davypegabryel@gmail.com>
2023-04-29 01:27:40 -0300

    Início

---
+++ a.py
@@ -0,0 +1,9 @@
+mais13 = 0
+while True:
+    idade = int(input('Insira a idade: '))
+    altura = float(input('Qual altura?: '))
+    if altura == 0:
+        break
+    if idade >= 13 and idade < 150:
+        mais13 += 1
+print('O número de alunos é: ',mais13)
