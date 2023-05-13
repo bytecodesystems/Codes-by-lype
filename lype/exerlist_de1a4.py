f5ff0f35a272201976a3a0a0179055f00300fe90
Phyllipe Gabryel <126884742+lypegabryel7@users.noreply.github.com>
2023-05-13 17:07:36 -0300

    Add files via upload

---
+++ lype/exerlist_de1a4.py
@@ -0,0 +1,15 @@
+from random import *
+
+#exercício 1
+#lista = [87,123,487,50,180]
+#for i in range (0, len(lista)):
+#    print(f'O elemento é {i} e sua posição na lista é {i}')
+
+#exercício 2
+lista_float = [random() for i in range (0,10)]
+print(lista_float)
+tamanho = len(lista_float)-1
+for i in range(tamanho, -1, -1):
+    print(lista_float[i])
+#exercício 3
+#exercício 4
---
+++ lype/lype.py
@@ -0,0 +1,4 @@
+import os
+from time import sleep
+
+matrix = [['.' for space in range(20)] for line in range(15)]
